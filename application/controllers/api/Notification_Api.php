5db23ca5dbe2c650b8fdf1ea43fb810410f69258
David <david@aimtech.am>
2019-12-06 14:49:29 +0400

    some fix 5

--- application/controllers/api/Notification_Api.php
+++ application/controllers/api/Notification_Api.php
@@ -76,8 +76,6 @@ class Notification_Api extends REST_Controller
 		}
 		$action_id = $this->input->post("action_id");
 
-		var_dump($res, $action_id);
-die;
 		if ($this->input->post("answer") == 1) {
 			$this->db->trans_start();
 
