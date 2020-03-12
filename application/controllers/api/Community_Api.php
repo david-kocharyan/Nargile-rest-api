98a6dc58ebd5ad3963150a630dc649b739720b32
David <david@aimtech.am>
2020-03-12 17:20:35 +0400

    coin offers fix 3

--- application/controllers/api/Community_Api.php
+++ application/controllers/api/Community_Api.php
@@ -242,7 +242,7 @@ class Community_Api extends REST_Controller
 	private function join()
 	{
 		$this->db->join("area", "area.id = restaurants.area_id");
-		$this->db->join("countries", "countries.id = coin_offers.country");
+		$this->db->join("countries", "countries.id = coin_offers.country", "left");
 	}
 
 	public function get_friends_get()
