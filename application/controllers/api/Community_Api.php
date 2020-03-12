64d16012eb5488584eb395523528ee80c289debd
David <david@aimtech.am>
2020-03-12 17:57:05 +0400

    coin offers fix 11

--- application/controllers/api/Community_Api.php
+++ application/controllers/api/Community_Api.php
@@ -129,9 +129,9 @@ class Community_Api extends REST_Controller
 		$this->db->where("coin_offers.count > 0");
 
 		if ($user->region_id != NULL) {
-			$this->db->where("coin_offers.region = $user->region_id");
+			$this->db->where(array('coin_offers.region' => $user->region_id));
 		} else{
-			$this->db->where("coin_offers.country = $user->country");
+			$this->db->where(array('coin_offers.country' => $user->country));
 		}
 
 		$this->db->where("DATE(FROM_UNIXTIME(coin_offers.valid_date)) >= CURDATE()");
